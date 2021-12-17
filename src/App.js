31114a69ecbce9fa615a814197e6ccfcb0be2701
sharath V <sharathkrml@gmail.com>
2021-12-17 14:36:00 +0530

    added routing to all pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Destination from "./components/Destination/Destination";
+import Crew from "./components/Crew/Crew";
+import Technology from "./components/Technology/Technology";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/destination">Destination</Link>
+            </li>
+            <li>
+              <Link to="/crew">Crew</Link>
+            </li>
+            <li>
+              <Link to="/technology">Technology</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/technology" element={<Technology />}></Route>
+          <Route path="/crew" element={<Crew />}></Route>
+          <Route path="/destination" element={<Destination />}></Route>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
---
+++ src/components/Crew/Crew.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Crew() {
+    return (
+        <div>
+            Crew
+        </div>
+    )
+}
+
+export default Crew
