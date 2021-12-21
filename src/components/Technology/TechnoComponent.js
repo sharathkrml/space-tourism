20a53c6d296f371524a626980f7935e89cb6cb72
sharath V <sharathkrml@gmail.com>
2021-12-21 23:04:37 +0530

    added technologu desktop view

--- src/components/Technology/TechnoComponent.js
+++ src/components/Technology/TechnoComponent.js
@@ -1,10 +1,29 @@
-import React from "react";
-
+import "./TechnoComponent.scss";
 function TechnoComponent(props) {
   return (
     <div className="techno-component">
+      <div className="techno-buttons">
+        <button
+          className={props.current.toString() === "0" ? "active" : undefined}
+          onClick={() => props.goTo(0)}
+        >
+          1
+        </button>
+        <button
+          className={props.current.toString() === "1" ? "active" : undefined}
+          onClick={() => props.goTo(1)}
+        >
+          2
+        </button>
+        <button
+          className={props.current.toString() === "2" ? "active" : undefined}
+          onClick={() => props.goTo(2)}
+        >
+          3
+        </button>
+      </div>
       <div className="techno-details">
-        <div className="terminology">THE TERMINOLOGY</div>
+        <div className="terminology">THE TERMINOLOGY...</div>
         <div className="techno-name">{props.name}</div>
         <div className="techno-desc">{props.desc}</div>
       </div>
--- src/components/Technology/Technology.js
+++ src/components/Technology/Technology.js
@@ -14,6 +14,9 @@ const getIsMobile = () => window.innerWidth <= 768;
 
 function Technology() {
   const [isMobile, setIsMobile] = useState(getIsMobile());
+  const carouselRef = useRef(null);
+  const [currDest, setCurrDest] = useState(0);
+
   useEffect(() => {
     const onResize = () => {
       setIsMobile(getIsMobile());
@@ -23,26 +26,37 @@ function Technology() {
       window.removeEventListener("resize", onResize);
     };
   }, []);
+  const goTo = (n) => {
+    carouselRef.current.goTo(n);
+  };
   return (
     <div className="Technology">
       <div className="dest-banner">
         <strong>03</strong>&nbsp; SPACE LAUNCH 101
         <Carousel
+          onChange={(currentItem, pageIndex) => setCurrDest(pageIndex)}
+          ref={carouselRef}
           itemsToShow={1}
           className="techno-carousel"
           verticalMode={!isMobile}
         >
           <TechnoComponent
+            goTo={goTo}
+            current={currDest}
             name="Launch vehicle"
             desc=" A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad! "
             img={isMobile ? vehicleMob : vehicle}
           />
           <TechnoComponent
+            goTo={goTo}
+            current={currDest}
             name="Spaceport"
             desc="A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth's rotation for launch."
             img={isMobile ? spaceportMob : spaceport}
           />
           <TechnoComponent
+            goTo={goTo}
+            current={currDest}
             name="Space capsule"
             desc="A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
             img={isMobile ? capsuleMob : capsule}
