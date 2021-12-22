f4ed058ee7266c97d4238b5a416aad0f63052270
sharath V <sharathkrml@gmail.com>
2021-12-22 19:48:33 +0530

    added tablet and desktop view

--- src/components/Technology/TechnoComponent.js
+++ src/components/Technology/TechnoComponent.js
@@ -2,7 +2,7 @@ import "./TechnoComponent.scss";
 function TechnoComponent(props) {
   return (
     <div className="techno-component">
-      <div className="techno-buttons">
+      <div className="techno-buttons btn-desktop">
         <button
           className={props.current.toString() === "0" ? "active" : undefined}
           onClick={() => props.goTo(0)}
@@ -26,6 +26,26 @@ function TechnoComponent(props) {
         <div className="terminology">THE TERMINOLOGY...</div>
         <div className="techno-name">{props.name}</div>
         <div className="techno-desc">{props.desc}</div>
+      </div>
+      <div className="techno-buttons btn-mob">
+        <button
+          className={props.current.toString() === "0" ? "active" : undefined}
+          onClick={() => props.goTo(0)}
+        >
+          1
+        </button>
+        <button
+          className={props.current.toString() === "1" ? "active" : undefined}
+          onClick={() => props.goTo(1)}
+        >
+          2
+        </button>
+        <button
+          className={props.current.toString() === "2" ? "active" : undefined}
+          onClick={() => props.goTo(2)}
+        >
+          3
+        </button>
       </div>
       <div className="techno-image">
         <img src={props.img} alt={props.name} />
